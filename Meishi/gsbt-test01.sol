e811afd30f2d393c005dd0353f42704f508e8b21
Gun <124517257+gunjp@users.noreply.github.com>
2023-06-07 20:23:29 +0900

    Update gsbt-test01.sol

--- Meishi/gsbt-test01.sol
+++ Meishi/gsbt-test01.sol
@@ -3,28 +3,36 @@ pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts@4.9.0/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts@4.9.0/access/Ownable.sol";
+import "@openzeppelin/contracts@4.9.0/utils/Strings.sol";
 
-contract Gsbt_test is ERC1155, Ownable {
+contract Gsbt_test2 is ERC1155, Ownable {
     constructor() ERC1155("") {}
+    // CONST
+    using Strings for uint256;
+    string public name = "GUN MEISHI TEST02";
+    string public symbol = "GMT";
+    string private _baseUri;
+    string private _extension = ".json";
 
+    // URI
     function setURI(string memory newuri) public onlyOwner {
         _setURI(newuri);
     }
+    function setBaseUri(string memory baseUri) external onlyOwner {
+        _baseUri = baseUri;
+    }
+    function uri(uint256 tokenId) public view virtual override returns (string memory) {
+        return string(abi.encodePacked(_baseUri, tokenId.toString(), _extension));
+    }
 
-    function mint(address account, uint256 id, uint256 amount, bytes memory data)
-        public
-        onlyOwner
-    {
+    // MINT
+    function mint(address account, uint256 id, uint256 amount, bytes memory data) public onlyOwner {
         _mint(account, id, amount, data);
     }
-
     function mintBatch(address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data)
-        public
-        onlyOwner
-    {
+        public onlyOwner{
         _mintBatch(to, ids, amounts, data);
     }
-
     function AirDrop(address[] memory list, uint256 id) public onlyOwner {
         require(bytes(uri(id)).length != 0, "Not initialized");
         for (uint256 i = 0; i < list.length; i++) {
