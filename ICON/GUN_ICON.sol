e5b4bad67c7b679b3dfaa76693821d8b395c0fbf
Gun <124517257+gunjp@users.noreply.github.com>
2023-07-15 02:30:18 +0900

    Update and rename icon.sol to GUN_ICON.sol

---
+++ ICON/GUN_ICON.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract GUN_ICON is ERC721, Ownable {
+    constructor() ERC721("GUN_ICON", "GUN_ICON") {}
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "https://storage.gunjp.xyz/ICON";
+    }
+
+    function safeMint(address to, uint256 tokenId) public onlyOwner {
+        _safeMint(to, tokenId);
+    }
+}
